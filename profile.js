4cd79e6b5c728445b66dc6b7b344393d7581bc9c
HoangDungxz <54103175+HoangDungxz@users.noreply.github.com>
2019-10-21 00:03:43 +0700

    Add files via upload

---
+++ profile.js
@@ -0,0 +1,185 @@
+function hienbg(){
+	document.getElementById("menu1").style.transform = "scale(1)"; 
+	document.getElementById("menu1").style.background="none"; 
+	document.getElementById("menu1-a").style.color="rgba(255, 255, 255,0.5 )"; 
+
+	document.getElementById("menu2").style.transform = "scale(1)"; 
+	document.getElementById("menu2").style.background="none"; 
+	document.getElementById("menu2-a").style.color="rgba(255, 255, 255,0.5 )"; 
+
+	document.getElementById("menu3").style.transform = "scale(1)"; 
+	document.getElementById("menu3").style.background="none"; 
+	document.getElementById("menu3-a").style.color="rgba(255, 255, 255,0.5 )"; 
+
+	document.getElementById("menu4").style.transform = "scale(1)"; 
+	document.getElementById("menu4").style.background="none"; 
+	document.getElementById("menu4-a").style.color="rgba(255, 255, 255,0.5 )"; 
+
+	document.getElementById("menu5").style.transform = "scale(1)"; 
+	document.getElementById("menu5").style.background="none"; 
+	document.getElementById("menu5-a").style.color="rgba(255, 255, 255,0.5 )"; 
+
+	document.getElementById("header-ab").style.display="block";	
+	document.getElementById("header-ab-about").style.display="none";	
+	document.getElementById("header-ab-ex").style.display="none";	
+	document.getElementById("header-ab-skill").style.display="none";
+	document.getElementById("header-ab-awards").style.display="none";	
+	document.getElementById("header-ab-contact").style.display="none";	
+
+}
+
+
+function hien1(){
+	document.getElementById("menu1").style.transform = "scale(1.3)"; 
+	document.getElementById("menu1").style.background="rgba(70, 187, 280,1 )"; 
+	document.getElementById("menu1-a").style.color="white";	
+
+	document.getElementById("menu2").style.transform = "scale(1)"; 
+	document.getElementById("menu2").style.background="none"; 
+	document.getElementById("menu2-a").style.color="rgba(255, 255, 255,0.5 )"; 
+
+	document.getElementById("menu3").style.transform = "scale(1)"; 
+	document.getElementById("menu3").style.background="none"; 
+	document.getElementById("menu3-a").style.color="rgba(255, 255, 255,0.5 )"; 
+
+	document.getElementById("menu4").style.transform = "scale(1)"; 
+	document.getElementById("menu4").style.background="none"; 
+	document.getElementById("menu4-a").style.color="rgba(255, 255, 255,0.5 )"; 
+
+	document.getElementById("menu5").style.transform = "scale(1)"; 
+	document.getElementById("menu5").style.background="none"; 
+	document.getElementById("menu5-a").style.color="rgba(255, 255, 255,0.5 )"; 
+
+	document.getElementById("header-ab").style.display="none";	
+	document.getElementById("header-ab-about").style.display="block";	
+	document.getElementById("header-ab-ex").style.display="none";	
+	document.getElementById("header-ab-skill").style.display="none";
+	document.getElementById("header-ab-awards").style.display="none";	
+	document.getElementById("header-ab-contact").style.display="none";	
+
+
+}	function hien2(){
+	document.getElementById("menu2").style.transform = "scale(1.3)"; 
+	document.getElementById("menu2").style.background="rgba(70, 187, 280,1 )"; 
+	document.getElementById("menu2-a").style.color="white"; 
+
+	document.getElementById("menu1").style.transform = "scale(1)"; 
+	document.getElementById("menu1").style.background="none"; 
+	document.getElementById("menu1-a").style.color="rgba(255, 255, 255,0.5 )"; 
+
+	document.getElementById("menu3").style.transform = "scale(1)"; 
+	document.getElementById("menu3").style.background="none"; 
+	document.getElementById("menu3-a").style.color="rgba(255, 255, 255,0.5 )"; 
+
+	document.getElementById("menu4").style.transform = "scale(1)"; 
+	document.getElementById("menu4").style.background="none"; 
+	document.getElementById("menu4-a").style.color="rgba(255, 255, 255,0.5 )"; 
+
+	document.getElementById("menu5").style.transform = "scale(1)"; 
+	document.getElementById("menu5").style.background="none"; 
+	document.getElementById("menu5-a").style.color="rgba(255, 255, 255,0.5 )"; 
+
+	document.getElementById("header-ab").style.display="none";	
+	document.getElementById("header-ab-about").style.display="none";	
+	document.getElementById("header-ab-ex").style.display="block";
+	document.getElementById("header-ab-skill").style.display="none";	
+	document.getElementById("header-ab-awards").style.display="none";	
+	document.getElementById("header-ab-contact").style.display="none";	
+
+
+
+}	function hien3(){
+	document.getElementById("menu3").style.transform = "scale(1.3)"; 
+	document.getElementById("menu3").style.background="rgba(70, 187, 280,1 )"; 
+	document.getElementById("menu3-a").style.color="white"; 
+
+	document.getElementById("menu1").style.transform = "scale(1)"; 
+	document.getElementById("menu1").style.background="none"; 
+	document.getElementById("menu1-a").style.color="rgba(255, 255, 255,0.5 )"; 
+
+	document.getElementById("menu2").style.transform = "scale(1)"; 
+	document.getElementById("menu2").style.background="none"; 
+	document.getElementById("menu2-a").style.color="rgba(255, 255, 255,0.5 )"; 
+
+	document.getElementById("menu4").style.transform = "scale(1)"; 
+	document.getElementById("menu4").style.background="none"; 
+	document.getElementById("menu4-a").style.color="rgba(255, 255, 255,0.5 )"; 
+
+	document.getElementById("menu5").style.transform = "scale(1)"; 
+	document.getElementById("menu5").style.background="none"; 
+	document.getElementById("menu5-a").style.color="rgba(255, 255, 255,0.5 )"; 
+
+	document.getElementById("header-ab").style.display="none";	
+	document.getElementById("header-ab-about").style.display="none";	
+	document.getElementById("header-ab-ex").style.display="none";
+	document.getElementById("header-ab-skill").style.display="block";	
+	document.getElementById("header-ab-awards").style.display="none";	
+	document.getElementById("header-ab-contact").style.display="none";	
+
+
+
+
+
+}	function hien4(){
+	document.getElementById("menu4").style.transform = "scale(1.3)"; 
+	document.getElementById("menu4").style.background="rgba(70, 187, 280,1 )"; 
+	document.getElementById("menu4-a").style.color="white"; 
+
+	document.getElementById("menu1").style.transform = "scale(1)"; 
+	document.getElementById("menu1").style.background="none"; 
+	document.getElementById("menu1-a").style.color="rgba(255, 255, 255,0.5 )"; 
+
+	document.getElementById("menu3").style.transform = "scale(1)"; 
+	document.getElementById("menu3").style.background="none"; 
+	document.getElementById("menu3-a").style.color="rgba(255, 255, 255,0.5 )"; 
+
+	document.getElementById("menu2").style.transform = "scale(1)"; 
+	document.getElementById("menu2").style.background="none"; 
+	document.getElementById("menu2-a").style.color="rgba(255, 255, 255,0.5 )"; 
+
+	document.getElementById("menu5").style.transform = "scale(1)"; 
+	document.getElementById("menu5").style.background="none"; 
+	document.getElementById("menu5-a").style.color="rgba(255, 255, 255,0.5 )"; 
+
+	document.getElementById("header-ab").style.display="none";	
+	document.getElementById("header-ab-about").style.display="none";	
+	document.getElementById("header-ab-ex").style.display="none";	
+	document.getElementById("header-ab-skill").style.display="none";	
+	document.getElementById("header-ab-awards").style.display="block";	
+	document.getElementById("header-ab-contact").style.display="none";	
+
+	
+
+
+
+}	function hien5(){
+	document.getElementById("menu5").style.transform = "scale(1.3)"; 
+	document.getElementById("menu5").style.background="rgba(70, 187, 280,1 )"; 
+	document.getElementById("menu5-a").style.color="white"; 
+
+	document.getElementById("menu1").style.transform = "scale(1)"; 
+	document.getElementById("menu1").style.background="none"; 
+	document.getElementById("menu1-a").style.color="rgba(255, 255, 255,0.5 )"; 
+
+	document.getElementById("menu3").style.transform = "scale(1)"; 
+	document.getElementById("menu3").style.background="none"; 
+	document.getElementById("menu3-a").style.color="rgba(255, 255, 255,0.5 )"; 
+
+	document.getElementById("menu4").style.transform = "scale(1)"; 
+	document.getElementById("menu4").style.background="none"; 
+	document.getElementById("menu4-a").style.color="rgba(255, 255, 255,0.5 )"; 
+
+	document.getElementById("menu2").style.transform = "scale(1)"; 
+	document.getElementById("menu2").style.background="none"; 
+	document.getElementById("menu2-a").style.color="rgba(255, 255, 255,0.5 )"; 
+
+	document.getElementById("header-ab").style.display="none";	
+	document.getElementById("header-ab-about").style.display="none";
+	document.getElementById("header-ab-ex").style.display="none";	
+	document.getElementById("header-ab-skill").style.display="none";	
+	document.getElementById("header-ab-awards").style.display="none";	
+	document.getElementById("header-ab-contact").style.display="block";	
+
+
+}		
+
